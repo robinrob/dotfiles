b73e0b127903a27ec4a235d2fb40bc9496f62dfc
msl <msl@mercury.local>
2013-10-06 19:40:01 +0100

    Updates.

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 ############################
 # .make.sh
@@ -9,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc"    # list of files/folders to symlink in homedir
+files="zshrc yadr bashrc vimrc zshrc zsh.prompts"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -30,16 +29,3 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
-=======
-#!/bin/sh
-
-if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone https://github.com/skwp/dotfiles.git "$HOME/.yadr"
-    cd "$HOME/.yadr"
-    [ "$1" == "ask" ] && export ASK="true"
-    rake install
-else
-    echo "YADR is already installed"
-fi
->>>>>>> e46601ec761edb5652103e8003c2ca664cdd42e1
