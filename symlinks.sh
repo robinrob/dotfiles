1f6d8ecd4aab376606d950716179b9d0b450931b
msl <robin@mrrobinsmith.com>
2014-06-21 19:50:21 -0700

    Auto-update

--- symlinks.sh
+++ symlinks.sh
@@ -2,8 +2,8 @@
 
 RUBYMINE_CONFIG=~/Library/Preferences/RubyMine50/keymaps/Robin.xml
 rm -rf $RUBYMINE_CONFIG
-ln -s $DOTFILES_HOME/Jetbrains-Robin.xml $RUBYMINE_CONFIG
+cp $DOTFILES_HOME/Jetbrains-Robin.xml $RUBYMINE_CONFIG
 
 PYCHARM_CONFIG=~/Library/Preferences/PyCharm30/keymaps/Robin.xml
 rm -rf $PYCHARM_CONFIG
-ln -s $DOTFILES_HOME/Jetbrains-Robin.xml $PYCHARM_CONFIG
+cp $DOTFILES_HOME/Jetbrains-Robin.xml $PYCHARM_CONFIG
