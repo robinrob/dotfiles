0b84aa73b17789e39225cb540b9ad96df1ffd15e
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-16 02:47:12 -0700

    Auto-update

--- jetbrains.sh
+++ jetbrains.sh
@@ -8,7 +8,7 @@ function install() {
 	yellow "\t${ITEM}Installing $1 config ..."
 	cp $2 $2.backup 2> /dev/null
 	rm -f $2
-	ln $DOTFILES_HOME/Robin.xml $2
+	ln Robin.xml $2
 }
 
 green "Installing Jetbrains IDE config files ..."
