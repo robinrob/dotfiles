3b9093218ef82ac655adb3035c663496bc9d9cd8
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-19 02:41:02 -0700

    Auto-update

--- functions.zsh
+++ functions.zsh
@@ -318,7 +318,7 @@ function bb {
 	result=`ls -d .git 2> /dev/null`
 	if [ "$result" ]
 	then
-		repo=`git remote show origin | grep "Fetch URL:" | awk '{split($3,a,"/"); print a[2]}'`
+		repo=`git config --get remote.origin.url | awk '{split($1,a,"/"); print a[2]}'`
 		green "Repo found: $repo"
 		url="https://bitbucket.org/robinrob/$repo"
 	else
