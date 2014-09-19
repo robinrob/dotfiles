082c11606821cf6145d22f17c88599b1bbfeaefd
Robin Smith <robin@mrrobinsmith.com>
2014-09-19 16:48:45 -0700

    Auto-update.

--- functions.zsh
+++ functions.zsh
@@ -413,12 +413,13 @@ function silent_cp {
 function save_crontab {
 	CRON_NAME="$HOSTNAME.cron"
 	SAVE_PATH="$DOTFILES_HOME/$CRON_NAME"
+	TMP_PATH="${SAVE_PATH}.tmp"
 	
 	rm -f $SAVE_PATH
 	green "Saving crontab to $SAVE_PATH ..."
-	crontab -l > "tmp.cron"
-	silent_cp tmp.cron $SAVE_PATH
-	rm tmp.cron
+	crontab -l > $TMP_PATH
+	silent_cp $TMP_PATH $SAVE_PATH
+	rm $TMP_PATH
 }
 
 function save_jetbrains {
